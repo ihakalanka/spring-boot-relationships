4e56eef687c78700e3c24d39a884ed1fb7315ea2
ihakalanka <akalankaih19@gmail.com>
2023-01-24 01:33:19 +0530

    initial commit

---
+++ oneToOne/src/test/java/com/akalanka/oneToOne/OneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akalanka.oneToOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
