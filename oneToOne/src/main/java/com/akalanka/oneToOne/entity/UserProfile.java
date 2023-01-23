b5214ad024c78b6756b318167c61d39a02a48d8f
ihakalanka <akalankaih19@gmail.com>
2023-01-24 02:53:09 +0530

    classes created

--- oneToOne/src/main/java/com/akalanka/oneToOne/entity/UserProfile.java
+++ oneToOne/src/main/java/com/akalanka/oneToOne/entity/UserProfile.java
@@ -1,2 +1,38 @@
-package com.akalanka.oneToOne.entity;public class UserProfile {
+package com.akalanka.oneToOne.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_profile")
+public class UserProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "address")
+    private String address;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+    @Column(name = "dob")
+    private LocalDate dob;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL , mappedBy = "userProfile")
+    private User user;
+
+    public UserProfile(String phone, String address, Gender gender, LocalDate dob) {
+        this.phone = phone;
+        this.address = address;
+        this.gender = gender;
+        this.dob = dob;
+    }
 }
